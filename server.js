f54b305790f1f76f6ae625ecfc46fe5f83356f34
tknemuru <kojikoji.max.5252@gmail.com>
2015-03-25 01:46:53 +0900

    test done

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 ﻿var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json({ extended: true }));
 
 app.get('/', function (req, res) {
     res.send('Hello, World!');
 });
 
-app.listen(process.env.PORT || 5000);
+app.post('/', function (req, res) {
+    console.log(req.body.title);
+})
+
+module.exports = app;
+
+if (!module.parent) {
+    app.listen(process.env.PORT || 5000);
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+﻿var request = require('supertest')
+var app = require('./server');
+
+describe('My API tests', function () {   
+    it('POSTでbodyのJSON情報が取得できる', function () {
+        request(app)
+        .post('/')
+        .send({ title: 'test title' })
+        .end(function (res) {
+            expect(res).to.exist;
+            expect(res.status).to.equal(200);
+        })
+    });
+});
