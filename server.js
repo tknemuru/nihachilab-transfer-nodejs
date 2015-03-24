a94466e0da21383cbc87c2007f810ed93fadb719
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-03-24 14:36:04 +0000

    express hello world

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
+});
+
+app.listen(1337);
